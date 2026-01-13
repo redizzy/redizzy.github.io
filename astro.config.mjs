35fca60069fdc47a288d15a0bcc37b538dae682b
DizzyL <14840345+dizzyl@user.noreply.gitee.com>
2026-01-13 20:51:01 +0800

    fix astro config for github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://redizzy.github.io',
-  integrations: [mdx(), sitemap(), tailwind()]
+  base: '/',
+  integrations: [mdx(), tailwind()]
 });
